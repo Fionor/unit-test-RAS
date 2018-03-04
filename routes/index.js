493b1dc33626d93a9c6074c8a1490891d61a6f4f
Dmitry <kdp1999@yandex.ru>
2018-03-04 11:15:24 +0200

    add GET /onetime_token & POST /user routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,15 @@ const router = express.Router();
 
 const ctrl_token = require('../controllers/token');
 const ctrl_client = require('../controllers/client');
+const ctrl_onetime_token = require('../controllers/onetime_token');
+const ctrl_user = require('../controllers/user');
 
 router.post('/token', ctrl_token.get_token);
 
 router.post('/client', ctrl_client.create_client);
+
+router.get('/onetime_token', ctrl_onetime_token.get_token);
+
+router.post('/user', ctrl_user.register_user);
 
 module.exports = router;
