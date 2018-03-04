bc997b9ee9a662bf659b6c83ab0120b67f0e680d
Dmitry <kdp1999@yandex.ru>
2018-03-04 11:12:51 +0200

    add redis & resourse_server params

--- config.js
+++ config.js
@@ -1,6 +1,18 @@
 module.exports = {
-    aouth: {
+    oauth: {
         db_uri: 'mongodb://localhost:27017/unit-test-ras',
         port: 3001,
+        url: '127.0.0.1'
+    },
+    redis: {
+        port: 6379,
+        host: '127.0.0.1',
+        family: 4,
+        password: '',
+        db: 0
+    },
+    resourse_server: {
+        port: 3000,
+        url: '127.0.0.1'
     }
 }
