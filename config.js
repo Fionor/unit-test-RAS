f48ec1b6d8871e2001d354ee1d4db69c9a578399
Dmitry <kdp1999@yandex.ru>
2018-05-30 17:13:37 +0300

    init env schema & change db  connector

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
     oauth: {
-        db_uri: 'mongodb://localhost:27017/unit-test-ras',
         port: 3001,
         url: '127.0.0.1',
         access_token_expires: 300, // 5m
